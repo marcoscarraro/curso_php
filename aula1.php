865f7ee0c02803d6340508b6025d406ae7b067f5
marcoscarraro <marcos.g.carraro@gmail.com>
2024-06-18 21:57:33 -0300

    Primeiro commit no github

---
+++ aula1.php
@@ -0,0 +1,35 @@
+<?php
+/* Aula de número 1 dia 18-06-2024
+Turma BG05000 
+Conteudos: echo, comentarios, variaveis
+Por: Marcos Carraro
+*/
+
+// Este é um comentário do código, não será exibido em tela.
+echo "Buenassss!!!!!<br>";
+echo "<br>";
+# Outro tipo de comentário
+
+/* Mais um tipo de comentário */
+
+$mensagemBoasVindas = "<br>Pequeno texto que você está lendo sobre o livro do Menino Maluquinho!";
+echo $mensagemBoasVindas;
+
+echo "<br>Outro texto:<br>Primeira linha<br>Segunda Linha<br>";
+
+$outraVariavel = "Outro grande texto!"; // Aqui é um comentário
+echo $outraVariavel;
+
+echo "<br>Calculos matemáticos<br>";
+echo 1+1;
+echo "<br>";
+echo 10-7;
+echo "<br>";
+echo 99*180;
+echo "<br>";
+echo 50/7;
+echo "<br>";
+echo 4%2;
+echo "<br>";
+echo 5%2;
+?>
